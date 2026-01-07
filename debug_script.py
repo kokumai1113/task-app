4275fbd2624d9ed84d512685406ce667c76ac02e
kokumai1113 <kokumai1113@gmail.com>
2026-01-07 18:49:39 +0900

    workout appの内容をコピーしております

--- debug_script.py
+++
@@ -1,9 +0,0 @@
-
-from notion_client import Client
-
-try:
-    client = Client(auth="dummy")
-    print("Type of client.databases:", type(client.databases))
-    print("Attributes of client.databases:", dir(client.databases))
-except Exception as e:
-    print(f"Error during inspection: {e}")
